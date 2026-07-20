eb0a1a82fc29cd18aafd9ec01efb1a694de679b1
Nona Yorn <yornnonakh@gmail.com>
2026-07-20 20:19:02 +0700

    update the new feature

--- lib/feature/auth/presentation/binding/register_binding.dart
+++ lib/feature/auth/presentation/binding/register_binding.dart
@@ -1,17 +1,30 @@
 import 'package:get/get.dart';
-import '../../../../core/network/api_client.dart';
-import '../../../folders/data/repositories/folder_repository_impl.dart';
-import '../../../folders/domain/repositories/folder_repository.dart';
+import 'package:note_app/core/network/api_client.dart';
+import 'package:note_app/core/storage/token_storage.dart';
+import 'package:note_app/feature/auth/data/repositories/auth_repository_impl.dart';
+import 'package:note_app/feature/auth/domain/repositories/auth_repository.dart';
+
+import '../controller/register_controller.dart';
 
 class RegisterBinding extends Bindings {
   @override
   void dependencies() {
-    if (!Get.isRegistered<FolderRepository>()) {
-      Get.put<FolderRepository>(
-        FolderRepositoryImpl(
+    if (!Get.isRegistered<AuthRepository>()) {
+      Get.put<AuthRepository>(
+        AuthRepositoryImpl(
           apiClient: Get.find<ApiClient>(),
+          tokenStorage: Get.find<TokenStorage>(),
         ),
         permanent: true,
+      );
+    }
+
+    if (!Get.isRegistered<RegisterController>()) {
+      Get.lazyPut<RegisterController>(
+            () => RegisterController(
+              authRepository: Get.find<AuthRepository>(),
+            ),
+        fenix: false,
       );
     }
   }
