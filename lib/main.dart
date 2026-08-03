5885f4c7d0de6777a3a8d266c7a3a21e1fe1dc22
apple <apple@apples-Mac-mini.local>
2026-08-03 16:46:55 +0700

    update the new feature

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'app/data/services/theme_service.dart';
   App: Otokhi Notes
   Date: 08.02.2026
   Update by: branch nona_developer
+  Update by: branch nona
   Feature: dark mode and light mode, rebranding to Otokhi
  */
 
